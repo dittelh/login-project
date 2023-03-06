3eee683bbe4840bc85f3159695ba14a3e5e84aac
Ditte Lykke Hansen <dittehansen143@gmail.com>
2023-03-06 21:47:29 +0100

    just trying to learn

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+// Destroy the logged-in sessions and redirect the user to the login page.
+
+session_start();
+session_destroy();
+// Redirect to the login page:
+header('Location: index.html');
+
+?>
